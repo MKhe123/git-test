4802b11498ac06a7decb5bf3ac813841d5ef29f1
KK <kk123@123.com>
2021-05-11 15:30:37 +0800

    first Commit

---
+++ src/main/java/com/xxx/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.xxx.git;
+
+/**
+ * @Auther: Koreleone
+ * @Date: 2021-05-11 15:13
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,Git");
+        System.out.println("hello,Git");
+    }
+}
